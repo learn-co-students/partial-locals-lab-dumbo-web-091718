2ae4a2b7a42af95c4695cc095c5b88fe2b8ad679
DonovanDwyer <dwyer.donovan@gmail.com>
2018-10-20 11:46:21 -0400

    added search functionality

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(str)
+    if str.downcase == "all" || str.empty?
+      return self.all
+    else
+      self.all.select do |student|
+        student.name.downcase.include?(str)
+      end
+    end
+  end
 end
